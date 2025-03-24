8d585c31ceec2b960d47a1ee8d4d8e0e1224ec96
marcossilva11 <marcosant2170@gmail.com>
2025-03-24 16:19:32 -0300

    nova classe Titular criada

---
+++ CaixaEletronico/Titular.cs
@@ -0,0 +1,13 @@
+﻿class Titular
+{
+    public Titular(string nome, string cpf, int idade)
+    {
+        Nome = nome;
+        Cpf = cpf;
+        Idade = idade;
+    }
+
+    public string Nome { get; }
+    public string Cpf { get; }
+    public int Idade { get; }
+}
